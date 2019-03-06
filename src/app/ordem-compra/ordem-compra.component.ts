50e3a877017c321315f9d3d82fd687bbbace65dc
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-03-06 10:49:19 -0300

    Developing Form to ordemCompraComponent

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string;
+  public numero: string;
+  public complemento: string;
+  public formaPagamento: string;
+
   constructor() { }
 
   ngOnInit() {
+    // eyms
+    this.endereco = '';
+    this.numero = '';
+    this.complemento = '';
+    this.formaPagamento = '';
   }
 
+  public atualizaEndereco(enderecoInput: string): void {
+    this.endereco = enderecoInput;
+  }
+
+  public atualizaNumero(numeroInput: string): void {
+    this.numero = numeroInput;
+  }
+
+  public atualizaComplemento(complementoInput: string): void {
+    this.complemento = complementoInput;
+  }
+
+  public atualizaFormaPagamento(formaPagamentoInput: string): void {
+    this.formaPagamento = formaPagamentoInput;
+  }
 }
