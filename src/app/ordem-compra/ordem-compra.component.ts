a47cfd9176989dfc143ec6642757316a6dd56909
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-03-19 11:08:00 -0300

    Validating inputs using Two Way Biding
    Validating inputs using Two Way Biding

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -12,6 +13,18 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string;
   public formaPagamento: string;
 
+  // Controle de validação dos campos
+  public enderecoValido: boolean;
+  public numeroValido: boolean;
+  public complementoValido: boolean;
+  public formaPagamentoValido: boolean;
+
+  // Atributos para estado primitivos dos campos (pristine)
+  public enderecoEstadoPrimitivo = true;
+  public numeroEstadoPrimitivo = true;
+  public complementoEstadoPrimitivo = true;
+  public formaPagamentoEstadoPrimitivo = true;
+
   constructor() { }
 
   ngOnInit() {
@@ -24,17 +37,29 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(enderecoInput: string): void {
     this.endereco = enderecoInput;
+    this.enderecoEstadoPrimitivo = false;
+
+    this.enderecoValido = this.endereco.trim().length > 3 ? true : false;
   }
 
   public atualizaNumero(numeroInput: string): void {
     this.numero = numeroInput;
+    this.numeroEstadoPrimitivo = false;
+
+    this.numeroValido = this.numero.trim() !== '' ? true : false;
   }
 
   public atualizaComplemento(complementoInput: string): void {
     this.complemento = complementoInput;
+    this.complementoEstadoPrimitivo = false;
+
+    this.complementoValido = this.complemento.trim() !== '' ? true : false;
   }
 
   public atualizaFormaPagamento(formaPagamentoInput: string): void {
     this.formaPagamento = formaPagamentoInput;
+    this.formaPagamentoEstadoPrimitivo = false;
+
+    this.formaPagamentoValido = this.formaPagamento.trim() !== '' ? true : false;
   }
 }
