d68015fd55f47861ec79fca4b28c5a9e67849b56
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-03-22 15:12:36 -0300

    Implementado os itens do carrinho no componente OrdemCompra
    Implementado os itens do carrinho no componente OrdemCompra

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -21,10 +21,58 @@ class CarrinhoService {
             1
         );
 
-        this.itens.push(itemCarrinho);
+        // Return a reference of the Array's element
+        const itemCarrinhoEncontrado = this.itens.find((item: ItemCarrinho) => item.id === itemCarrinho.id);
 
-        console.log('itemCarrinho', itemCarrinho);
-        console.log('Exibir Itens', this.exibirItens());
+        if (itemCarrinhoEncontrado) {
+
+            itemCarrinhoEncontrado.quantidade += 1;
+        } else {
+            // Insert item to the shopping cart.
+            this.itens.push(itemCarrinho);
+        }
+    }
+
+    public adicionarQuantidade(itemCarrinho: ItemCarrinho): void {
+
+        const itemCarrinhoEncontrado = this.itens.find((item: ItemCarrinho) => item.id === itemCarrinho.id);
+
+        if (itemCarrinhoEncontrado) {
+
+            itemCarrinhoEncontrado.quantidade += 1;
+        }
+    }
+
+    public diminuirQuantidade(itemCarrinho: ItemCarrinho): void {
+
+        const itemCarrinhoEncontrado = this.itens.find((item: ItemCarrinho) => item.id === itemCarrinho.id);
+
+        if (itemCarrinhoEncontrado) {
+
+            itemCarrinhoEncontrado.quantidade -= 1;
+
+            // Remove the item of the Shopping Cart
+            if (itemCarrinhoEncontrado.quantidade === 0) {
+
+                const x = this.itens.splice(this.itens.indexOf(itemCarrinhoEncontrado), 1);
+            }
+        }
+    }
+
+    public totalCarrinhoCompras(): number {
+
+        let total = 0;
+
+        this.itens.map((item: ItemCarrinho) => {
+            total = total + (item.quantidade * item.valor);
+        });
+
+        return total;
+    }
+
+    public limparCarrinho(): void {
+
+        this.itens = [];
     }
 }
 
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -5,6 +5,7 @@ import { OrdemCompraService } from '../ordem-compra.service';
 import { CarrinhoService } from '../carrinho.service';
 
 import { Pedido } from '../shared/pedido.model';
+import { ItemCarrinho } from '../shared/item-carrinho.model';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -26,6 +27,7 @@ export class OrdemCompraComponent implements OnInit {
 
   // ID do Pedido
   public idPedidoCompra: number;
+  public itensCarrinho: ItemCarrinho[] = [];
 
   constructor(
     private ordemCompraService: OrdemCompraService,
@@ -34,37 +36,49 @@ export class OrdemCompraComponent implements OnInit {
 
   ngOnInit() {
 
-    console.log('Exibir Itens do Carrinho', this.carrinhoService.exibirItens());
-
+    this.itensCarrinho = this.carrinhoService.exibirItens();
   }
 
   public confirmarCompra(): void {
 
-    console.log(this.formulario);
-
     if (this.formulario.status === 'INVALID') {
-      console.log('Formulário está inválido.');
-
       this.formulario.get('endereco').markAsTouched();
       this.formulario.get('numero').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
     } else {
 
-      const pedido: Pedido = new Pedido(
-        this.formulario.value.endereco,
-        this.formulario.value.numero,
-        this.formulario.value.complemento,
-        this.formulario.value.formaPagamento
-      );
+      if (this.carrinhoService.exibirItens().length === 0) {
 
-      console.log('Formulário está Válido.');
+        alert('Você não selecionou nenhum item!');
 
-      this.ordemCompraService.efetivarCompra(pedido)
-        .subscribe((idPedido: number) => {
+      } else {
 
-          this.idPedidoCompra = idPedido;
-        });
+        const pedido: Pedido = new Pedido(
+          this.formulario.value.endereco,
+          this.formulario.value.numero,
+          this.formulario.value.complemento,
+          this.formulario.value.formaPagamento,
+          this.carrinhoService.exibirItens()
+        );
+
+        this.ordemCompraService.efetivarCompra(pedido)
+          .subscribe((idPedido: number) => {
+
+            this.idPedidoCompra = idPedido;
+            this.carrinhoService.limparCarrinho();
+          });
+      }
     }
+  }
+
+  public adicionar(item: ItemCarrinho): void {
+
+    this.carrinhoService.adicionarQuantidade(item);
+  }
+
+  public diminuir(item: ItemCarrinho): void {
+
+    this.carrinhoService.diminuirQuantidade(item);
   }
 
 }
