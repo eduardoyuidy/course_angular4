7019201999391c9661f9a3d2827b0a68df72a902
Eduardo Yuidy <eduardoyuidy@gmail.com>
2019-03-25 16:20:53 -0300

    Animation Banner Transition Translate
    Animation Banner Transition Translate (Left to Right)

--- app3/src/app/acesso/acesso.component.ts
+++ app3/src/app/acesso/acesso.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado',[
+        style({
+          transform: 'translate(-60px, 0px)',
+          opacity: 0
+        }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.estadoBanner = 'criado';
   }
 
 }
